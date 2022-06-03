2f4f5d0256feed57e13c502926c5c4c02dbf16ee
Daniru2007 <daniruayuka@gmail.com>
2022-06-03 17:48:04 +0530

    [FEATURE] setting up a player with gravity

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 import sys
+from engine import Entity
+
 pygame.init()
 
 WINDOW_SIZE = (600, 400)
@@ -15,31 +17,47 @@ class Point(object):
         self.x = x
         self.y = y
         self.def_y = y
+        self.time = 0
 
     def move(self, px, py):
-        if abs(py - self.def_y) < 50 and abs(px - self.x) < 100:
+        if abs(py - self.def_y) < 50 and abs(px - self.def_x) < 100:
             self.y += (py - self.y) / max(abs(px - self.x), 1)
+            self.time += 1
         else:
-            self.x += (self.def_x - self.x) / 20
             self.y += (self.def_y - self.y) / 20
+            self.time = 0
 
     def display(self, screen):
         pygame.draw.circle(screen, (255, 255, 255), (self.x, self.y), 1)
 
 
 running = True
+player = Entity(10, 10, 16, 16, "player")
+player.load_animations("animations/player.json")
+player.set_action("idle")
 objects = []
 for i in range(WINDOW_SIZE[0]):
     objects.append(Point(i, 200))
 while running:
+    player.gravity += 0.2
+    if player.gravity > 2:
+        player.gravity = 2
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                player.gravity = -3
     screen.fill((0, 0, 0))
     mx, my = pygame.mouse.get_pos()
     for object in objects:
         object.move(mx, my)
         object.display(screen)
+
+    player.movement[1] += player.gravity
+    player.move([])
+    player.display(screen, [0, 0])
     pygame.display.update()
     clock.tick(60)
 pygame.quit()
